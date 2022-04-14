87da8c846be396664faf02ac568dd54c239a1932
jadehuynh <jadehuynh08@gmail.com>
2022-04-14 16:45:55 -0500

    polishing license functions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,24 +5,42 @@ function renderLicenseBadge(license) {
     let licenseValue = answers.license;
     let selectedLicense = '';
 
-    if ( answers.license ===  ) {
-        return " ";
-    }else {
-        return getl;
+    if ( answers.license === 'Apache2.0') {
+        return selectedLicense ='![Apache](https://img.shields.io/badge/license-Apache2.0-red)';
+    }else if ( answers.license === 'MIT') {
+        return selectedLicense ='![MIT](https://img.shields.io/badge/license-MIT-red)';
+    }else if ( answers.license === 'ISC') {
+        return selectedLicense ='![ISC](https://img.shields.io/badge/license-ISC-red)';
+    }else if ( answers.license === 'CC') {
+        return selectedLicense ='![CC](https://img.shields.io/badge/license-CC-red)';
+    }else if ( answers.license === 'Boost') {
+        return selectedLicense ='![Boost](https://img.shields.io/badge/license-Boost-red)';
+    }else{
+        answers.license = "Not Applicable"
     }
-}
-
+    return selectedLicense;
+};
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-    var getUrl = "https://img.shields.io/badge/license-${answers.license}-red";
-    
-    if ( ${answers.license} === ${answers.license} ) {
-        return getUrl;
+    let licenseValue = answers.license;
+    let licenseLink = '';
+
+    if ( answers.license === 'Apache2.0') {
+        return licenseLink ='[Apache](https://img.shields.io/badge/license-Apache2.0-red)';
+    }else if ( answers.license === 'MIT') {
+        return licenseLink ='[MIT](https://img.shields.io/badge/license-MIT-red)';
+    }else if ( answers.license === 'ISC') {
+        return licenseLink ='[ISC](https://img.shields.io/badge/license-ISC-red)';
+    }else if ( answers.license === 'CC') {
+        return licenseLink ='[CC](https://img.shields.io/badge/license-CC-red)';
+    }else if ( answers.license === 'Boost') {
+        return licenseLink ='[Boost](https://img.shields.io/badge/license-Boost-red)';
     }else{
-        return " ";
+        answers.license = "Not Applicable"
     }
-}
+    return licenseLink;
+};
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
